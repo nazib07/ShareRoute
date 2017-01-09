2b2372b4b4eeb2bf3fb3df5cf2cf5867b479f6aa
A. B. M.Nazibullah <nazib.cse@gmail.com>
2017-01-09 23:39:25 +0600

    fileshare added, metadata updated

--- app/src/main/java/shareroute/nazib/com/shareroute/DetailsActivity.java
+++ app/src/main/java/shareroute/nazib/com/shareroute/DetailsActivity.java
@@ -1,6 +1,7 @@
 package shareroute.nazib.com.shareroute;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +9,10 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import java.io.File;
+
+import static shareroute.nazib.com.shareroute.FileUtils.getCreatedRouteFileObject;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -22,10 +27,13 @@ public class DetailsActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("SHAREROUTE", "share button clicked");
+                File file = getCreatedRouteFileObject("test1.geojson");
+
                 Intent sendIntent = new Intent();
                 sendIntent.setAction(Intent.ACTION_SEND);
                 sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
-                sendIntent.setType("*/*");
+                sendIntent.setType("text/geojson");
+                sendIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://"+file.getAbsolutePath()));
                 startActivity(sendIntent);
             }
         });
